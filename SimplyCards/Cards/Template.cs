15eb728780c3198c584f54f7e78f536ded43c18c
willuwontu <willuwontu3@gmail.com>
2021-10-18 14:38:12 -0700

    Part 1

---
+++ SimplyCards/Cards/Template.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnboundLib;
+using UnboundLib.Cards;
+using UnityEngine;
+
+namespace SimplyCards.Cards
+{
+    class Template : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers, Block block)
+        {
+            UnityEngine.Debug.Log($"[{SimplyCards.ModInitials}][Card] {GetTitle()} has been setup.");
+        }
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            UnityEngine.Debug.Log($"[{SimplyCards.ModInitials}][Card] {GetTitle()} has been added to player {player.playerID}.");
+        }
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            UnityEngine.Debug.Log($"[{SimplyCards.ModInitials}][Card] {GetTitle()} has been removed from player {player.playerID}.");
+        }
+
+        protected override string GetTitle()
+        {
+            return "CardName";
+        }
+        protected override string GetDescription()
+        {
+            return "CardDescription";
+        }
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Common;
+        }
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Effect",
+                    amount = "No",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                }
+            };
+        }
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.ColdBlue;
+        }
+        public override string GetModName()
+        {
+            return SimplyCards.ModInitials;
+        }
+    }
+}
--- SimplyCards/SimplyCards.cs
+++ SimplyCards/SimplyCards.cs
@@ -1,8 +1,39 @@
-﻿using System;
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using SimplyCards.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
 
 namespace SimplyCards
 {
-    public class SimplyCards
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class SimplyCards : BaseUnityPlugin
     {
+        private const string ModId = "com.willuwontu.rounds.SimplyCards";
+        private const string ModName = "Simply Cards";
+        public const string Version = "1.0.0"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "SC";
+
+        public static SimplyCards instance { get; private set; }
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            instance = this;
+            CustomCard.BuildCard<MyCardName>(); // Repeat for each card to be built
+        }
     }
 }
